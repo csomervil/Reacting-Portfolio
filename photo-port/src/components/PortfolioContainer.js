938d14c4029aed13f37a3a847ee4c69060029a39
csomervil <carson.somerville@mail.utoronto.ca>
2022-07-10 12:27:06 -0400

    starters and initial layout

---
+++ photo-port/src/components/PortfolioContainer.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Resume from './pages/Resume';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+  const renderPage = () => {
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
